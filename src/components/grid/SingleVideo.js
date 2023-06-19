1da58c85f7ef98bb2bd7d1d32f2a16c65790bf7f
mozahid <ce.muzahid@gmail.com>
2023-06-19 22:06:02 +0600

    create tag slice & tag API

--- src/components/grid/SingleVideo.js
+++ src/components/grid/SingleVideo.js
@@ -1,7 +1,8 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
-const SingleVideo = ({video = {}}) => { // default value
-  const {id, thumbnail, title, duration, author, avatar, views,date, likes, unlikes} = video
+const SingleVideo = ({ video = {} }) => {
+  // default value
+  const { id, thumbnail, title, duration, author, avatar, views, date } = video
   return (
     <>
       <div className="col-span-12 sm:col-span-6 md:col-span-3 duration-300 hover:scale-[1.03]">
@@ -22,23 +23,16 @@ const SingleVideo = ({video = {}}) => { // default value
 
           <div className="flex flex-row mt-2 gap-2">
             <Link to={`videos/${id}`} className="shrink-0">
-              <img
-                src={avatar}
-                className="rounded-full h-6 w-6"
-                alt={author}
-              />
+              <img src={avatar} className="rounded-full h-6 w-6" alt={author} />
             </Link>
 
             <div className="flex flex-col">
               <Link href="/video/1">
-                <p className="text-slate-900 text-sm font-semibold">
-                  {title}
-                </p>
+                <p className="text-slate-900 text-sm font-semibold">{title}</p>
               </Link>
               <Link
                 className="text-gray-400 text-xs mt-2 hover:text-gray-600"
-                href={`videos/${id}`}
-              >
+                href={`videos/${id}`}>
                 {author}
               </Link>
               <p className="text-gray-400 text-xs mt-1">
@@ -49,7 +43,7 @@ const SingleVideo = ({video = {}}) => { // default value
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default SingleVideo;
+export default SingleVideo
---
+++ src/features/tags/tagsAPI.js
@@ -0,0 +1,7 @@
+import axiosInstance from '../../utils/axios'
+
+export const getTags = async () => {
+  const response = await axiosInstance.get('/tags')
+
+  return response.data
+}
