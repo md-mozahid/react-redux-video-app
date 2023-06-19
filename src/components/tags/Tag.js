98ed91779182481bdda00b086cfe961280673c37
mozahid <ce.muzahid@gmail.com>
2023-06-19 22:24:53 +0600

    tags showing to UI

--- src/components/tags/Tag.js
+++ src/components/tags/Tag.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react'
 
-const Tag = () => {
+const Tag = ({ title }) => {
   return (
     <>
       <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">
-        react
+        {title}
       </div>
       {/* <div className="bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer">
         redux
       </div> */}
     </>
-  );
-};
+  )
+}
 
-export default Tag;
+export default Tag
--- src/components/tags/Tags.js
+++ src/components/tags/Tags.js
@@ -1,16 +1,25 @@
-import React from "react";
-import Tag from "./Tag";
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchTags } from '../../features/tags/tagsSlice'
+import Tag from './Tag'
 
 const Tags = () => {
-  return (
-    <>
-      <section>
-        <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 border-b overflow-y-auto">
-          <Tag />
-        </div>
-      </section>
-    </>
-  );
-};
+  const dispatch = useDispatch()
+  const { tags } = useSelector((state) => state.tags)
 
-export default Tags;
+  useEffect(() => {
+    dispatch(fetchTags())
+  }, [dispatch])
+
+  return tags?.length > 0 ? (
+    <section>
+      <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 border-b overflow-y-auto">
+        {tags.map((tag) => (
+          <Tag key={tag.id} title={tag.title} />
+        ))}
+      </div>
+    </section>
+  ) : null
+}
+
+export default Tags
