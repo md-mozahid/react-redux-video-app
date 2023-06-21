67a611257f9fb023a6450bdc37403f2fbf89a619
mozahid <ce.muzahid@gmail.com>
2023-06-21 15:22:02 +0600

    searching from search bar

--- src/components/navbar/Search.js
+++ src/components/navbar/Search.js
@@ -1,14 +1,38 @@
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useMatch, useNavigate } from 'react-router-dom'
+import { searched } from '../../features/filter/filterSlice'
+
 const Search = () => {
+  const dispatch = useDispatch()
+  const { search } = useSelector((state) => state.filter)
+  const [input, setInput] = useState(search)
+
+  const match = useMatch('/')
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(searched(input))
+
+    // if user is not in home page, redirect to home page
+    if (!match) {
+      navigate('/')
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         className="outline-none border-none mr-2"
         type="search"
         name="search"
         placeholder="Search"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
       />
     </form>
-  );
-};
+  )
+}
 
-export default Search;
+export default Search
